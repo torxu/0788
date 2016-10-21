c6541bed27781bc88dd9ea9e4e69515aa91f66c7
torxu <aitoroterocarpintero@gmail.com>
2016-10-21 14:00:24 +0200

    0178.b Añadida impresion de venta.

---
+++ EstacionDeTren.java
@@ -0,0 +1,40 @@
+
+/**
+ * Write a description of class TrainStation here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class EstacionDeTren
+{
+	private MaquinaDeTiquet maquina1;
+	private MaquinaDeTiquet maquina2;
+	private int totalDinero;
+
+	public EstacionDeTren()
+	{
+		maquina1 = new MaquinaDeTiquet(300);
+		maquina2 = new MaquinaDeTiquet(200);
+		totalDinero = 0;
+    }
+  
+    public void imprimirTotalDinero()
+	{
+		totalDinero = maquina1.getTotal();
+		totalDinero = totalDinero + maquina2.getTotal();
+        System.out.println(totalDinero);
+	}
+	
+	public int totalDinero()
+	{
+	    return totalDinero;
+    }
+    
+    public void imprimirVenta()
+    {
+        maquina1.introducirDinero(300);
+        maquina1.imprimirTiquet();
+        maquina2.introducirDinero(100);
+        maquina2.imprimirTiquet();
+    }
+}
